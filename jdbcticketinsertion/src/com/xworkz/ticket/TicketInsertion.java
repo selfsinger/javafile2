afd6fb2ec51bc484885b7fdc37ce1584dc4dca17
selfsinger <94238356+selfsinger@users.noreply.github.com>
2022-05-09 23:03:11 +0530

    Update TicketInsertion.java

--- jdbcticketinsertion/src/com/xworkz/ticket/TicketInsertion.java
+++ jdbcticketinsertion/src/com/xworkz/ticket/TicketInsertion.java
@@ -1,9 +1,6 @@
 package com.xworkz.ticket;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 
 public class TicketInsertion {
